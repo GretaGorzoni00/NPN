dd64c41fb5587930bebf5e4456e54c158d769e47
greta <greta>
2025-10-27 13:20:49 +0100

    script modificati

--- src/LR.py
+++ src/LR.py
@@ -8,30 +8,31 @@ import random
 import math
 import argparse
 
-def main(seed, y_train, y_test, X_train, X_test, output_path):
+def main(seed, y_train, y_test, X_train, X_test, output_path, key):
 
 	random.seed(seed)
 
 
-	df_train = pd.read_csv(y_train, sep="\t")
-	df_test = pd.read_csv(y_test, sep="\t")
+	df_train = pd.read_csv(y_train, sep=";")
+	df_test = pd.read_csv(y_test, sep=";")
 
 	y_train = np.array([1 if label == "yes" else 0 for label in df_train["construction"]])
 	y_test = np.array([1 if label == "yes" else 0 for label in df_test["construction"]])
 
-	emb_train = pd.read_pickle(X_train)["embeddings"].tolist()
-	emb_test = pd.read_pickle(X_test)["embeddings"].tolist()
+	emb_train = pd.read_pickle(X_train)
+	emb_test = pd.read_pickle(X_test)
+ 
+	for n in range(1,13):
+     
+		# print([emb.keys() for emb in emb_test])
+		X_train = np.array([np.array(emb[f"{key}_layer_{n}"]) for emb in emb_train])
+		X_test = np.array([np.array(emb[f"{key}_layer_{n}"]) for emb in emb_test])
+		print("Layer"+ str(n))	
 
-	for i in range (5):
-		current_state = math.floor(random.random()*100)
-		clf = LogisticRegression(random_state=current_state, max_iter=10000)
-
-		for n in range(1,23):
-			X_train = np.array([emb[n - 1] for emb in emb_train])
-			X_test = np.array([emb[n - 1] for emb in emb_test])
+		for i in range (3):
+			current_state = math.floor(random.random()*100)
+			clf = LogisticRegression(random_state=current_state, max_iter=10000)
 			
-			print("Layer"+ str(n))
-
 			clf.fit(X_train, y_train)
 		
 			preds = clf.predict(X_test)
@@ -43,10 +44,11 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	
 	parser.add_argument("--seed", default = 42)
-	parser.add_argument("--X_train", default ="data/output/embeddings/MB_embedding_layers_UNK.pkl")
-	parser.add_argument("--y_train", default = "data/data.tsv")
-	parser.add_argument("--X_test", default = "data/output/embeddings/MB_embedding_layers_UNK.pkl")
-	parser.add_argument("--y_test", default = "data/data.tsv")
+	parser.add_argument("--X_train", default ="data/output/embeddings/BERT_embedding_UNK_train.pkl")
+	parser.add_argument("--y_train", default = "data/output/embeddings/lemma_balanced_train_pred.csv")
+	parser.add_argument("--X_test", default = "data/output/embeddings/BERT_embedding_UNK_test.pkl")
+	parser.add_argument("--y_test", default = "data/output/embeddings/lemma_balanced_test_pred.csv")
 	parser.add_argument("-o", "--output_path", default = "data/output/predictions")
+	parser.add_argument("-k", "--key", default = "UNK")
 	args = parser.parse_args()
-	main(args.seed, args.y_train, args.y_test, args.X_train, args.X_test, args.output_path)
+	main(args.seed, args.y_train, args.y_test, args.X_train, args.X_test, args.output_path, args.key)
