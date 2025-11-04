691ea5a10a04e6fabad3ec270083aa2335b25319
greta <greta>
2025-11-04 12:37:17 +0100

    script

--- src/split_un_balanced.py
+++ src/split_un_balanced.py
@@ -4,7 +4,8 @@ import os
 import random
 from collections import defaultdict
 
-random.seed(42)
+
+
 
 costruzioni_A_file = "data/data_set/A_filtrato.csv"
 costruzioni_SU_file = "data/data_set/SU_filtrato.csv"
@@ -23,11 +24,17 @@ def read_csv(file_path, delimiter=";"):
         return [row for row in reader if len(row) > 0]
     
 # === Campionamento casuale ===
-def sample_rows(rows, n=150):
-    """Campiona n righe casuali da una lista, o tutte se la lista è più corta"""
-    if len(rows) <= n:
-        return rows
-    return random.sample(rows, n)
+def sample_rows(rows, n=150, num_seeds=5):
+    """Esegue più campionamenti casuali con diversi seed"""
+    samples = []
+    for i in range(num_seeds):
+        seed = math.floor(random.random() * 100)
+        random.seed(seed)
+        if len(rows) <= n:
+            samples.append(rows)
+        else:
+            samples.append(random.sample(rows, n))
+    return samples
 
 
 
@@ -40,24 +47,32 @@ def write_csv(file_path, rows, delimiter=";"):
         
 # === Divisione trainig e test ===
 # Così si evita che lo stesso lemma compaia solo nel train o solo nel test, mantenendo un equilibrio interno a ciascun lemma
-def split_rows_lemma_per_lemma(rows, train_ratio):
-    """Split per lemma 80/20"""
-    lemma_dict = defaultdict(list)
-    for r in rows:
-        lemma_dict[r["noun"].strip()].append(r)
+def split_rows_lemma_per_lemma(rows, train_ratio, num_seeds=5):
+    """Split per lemma 80/20, eseguito con diversi seed"""
+    
+    sample_train_test =[]
+    for i in range (num_seeds):
+        seed = math.floor(random.random()*100)
+        random.seed(seed)
+        
+        lemma_dict = defaultdict(list)
+        for r in rows:
+            lemma_dict[r["noun"].strip()].append(r)
 
-    train_rows = []
-    test_rows = []
+        train_rows = []
+        test_rows = []
 
-    for lemma, lemma_rows in lemma_dict.items():
-        n_train = int(len(lemma_rows) * train_ratio)
-        sampled_train = random.sample(lemma_rows, n_train)
-        sampled_test = [r for r in lemma_rows if r not in sampled_train]
+        for lemma, lemma_rows in lemma_dict.items():
+            n_train = int(len(lemma_rows) * train_ratio)
+            sampled_train = random.sample(lemma_rows, n_train)
+            sampled_test = [r for r in lemma_rows if r not in sampled_train]
 
-        train_rows.extend(sampled_train)
-        test_rows.extend(sampled_test)
+            train_rows.extend(sampled_train)
+            test_rows.extend(sampled_test)
+        
+        sample_train_test.append((train_rows, test_rows))
 
-    return train_rows, test_rows
+        return sample_train_test
 
 # === Lettura e campionamento (150 per ciascun file) ===
 costruzioni_A = sample_rows(read_csv(costruzioni_A_file), 150)
@@ -118,6 +133,41 @@ train2 = train_c2 + train_d2
 test2 = test_c2 + test_d2
 write_csv("data/data_set/train_test_lemma_setting/distr_unbalanced_train.csv", train2)
 write_csv("data/data_set/train_test_lemma_setting/distr_unbalanced_test.csv", test2)
+
+# === Scenario LEMMI===
+# Training costruzioni + distrattori lemmi comuni - test solo lemmi non associati a stessa label del training
+    
+    train_c, test_c = split_rows_lemma_per_lemma(costruzioni, train_ratio)
+    train_d, test_d = split_rows_lemma_per_lemma(distrattori, train_ratio)
+
+    # insiemi dei lemmi visti nel train
+    lemmi_train_costruzioni = set([r["noun"].strip() for r in train_c])
+    lemmi_train_distrattori = set([r["noun"].strip() for r in train_d])
+
+    # Test filtrato:
+    #    - se un lemma è nel train come costruzione → nel test può comparire solo come distrattore
+    #    - se un lemma è nel train come distrattore → nel test può comparire solo come costruzione
+    filtered_test_c = [r for r in test_c if r["noun"].strip() not in lemmi_train_costruzioni]
+    filtered_test_d = [r for r in test_d if r["noun"].strip() not in lemmi_train_distrattori]
+
+    # appaiono in entrambe le categorie ma con label diversa)
+    lemmi_comuni = lemmi_train_costruzioni.intersection(lemmi_train_distrattori)
+
+    # lemmi comuni ma etichettati con label diversa
+    extra_test_c = [r for r in test_c if r["noun"].strip() in lemmi_train_distrattori]
+    extra_test_d = [r for r in test_d if r["noun"].strip() in lemmi_train_costruzioni]
+
+    test_c_final = filtered_test_c + extra_test_c
+    test_d_final = filtered_test_d + extra_test_d
+
+
+    train_final = train_c + train_d
+    test_final = test_c_final + test_d_final
+
+    print(f"Costruzioni train/test: {len(train_c)} / {len(test_c_final)}")
+    print(f"Distrattori train/test: {len(train_d)} / {len(test_d_final)}")
+
+    return train_final, test_final
 
 # === Scenario 3: training solo lemmi distrattori ===
 # Training: distrattori + costruzioni che non sono comuni
