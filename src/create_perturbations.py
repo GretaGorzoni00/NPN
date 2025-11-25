4f30da9774785d3286e56502df4889fe9934cffd
ellepannitto <ellepannitto@gmail.com>
2025-11-25 16:47:20 +0100

    update create_perturbations

--- src/create_perturbations.py
+++ src/create_perturbations.py
@@ -9,19 +9,26 @@ for filename in sys.argv[1:]:
 
 	with open(filename, encoding="utf-8") as fin:
 		csvfile = csv.DictReader(fin, delimiter=";")
-		writers = {x:csv.DictWriter(open(f"data/data_set/perturbed/{basename[:-4]}_{x}.csv", "w", encoding="utf-8"),
-                    fieldnames=csvfile.fieldnames,
-                    delimiter=";")
-    				for x in perturbations}
+		writers = {
+    		x : csv.DictWriter(open(f"data/data_set/perturbed/{basename[:-4]}_{x}.csv", "w", encoding="utf-8"),
+        			fieldnames=csvfile.fieldnames,
+                    delimiter=";"
+                    )
+    		for x in perturbations
+        }
 		for row in csvfile:
+
 			noun1, prep, noun2 = row["costr"].strip().split(" ")
 			for x in writers:
+				new_costr = row["costr"]
 				if x == "PNN":
-					row["costr"] = f"{prep} {noun1} {noun2}"
+					new_costr = f"{prep} {noun1} {noun2}"
 				if x == "PN":
-					row["costr"] = f"{prep} {noun1}"
+					new_costr = f"{prep} {noun1}"
 				if x == "NNP":
-					row["costr"] = f"{noun1} {noun2} {prep}"
+					new_costr = f"{noun1} {noun2} {prep}"
 				if x == "NP":
-					row["costr"] = f"{noun1} {prep}"
+					new_costr = f"{noun1} {prep}"
+				if row["construction"] == "yes":
+					row["costr"] = new_costr
 				writers[x].writerow(row)
